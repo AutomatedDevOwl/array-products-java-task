bc33b418e9ff5d37b3695f347a0fba911ed4566f
AutomatedDevOwl <40467074+AutomatedDevOwl@users.noreply.github.com>
2019-08-28 19:31:10 +0300

    Create ArrayProductsTests.java

---
+++ src/test/java/ArrayProductsTests.java
@@ -0,0 +1,15 @@
+package com.github.automatedowl.examples;
+
+import org.testng.annotations.Test;
+
+public class ArrayProductsTests {
+
+    @Test
+    public void arrayProductsTest() {
+       log("Running arrayProductsTest..");
+    }
+
+    void log(String logText) {
+        System.out.println(logText);
+    }
+}
